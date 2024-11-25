2fee118e32cf3cee1811a09f28456c345a782876
Khemrin <senkhemrin@gmail.com>
2024-11-25 16:15:07 +0700

    v1.0.3

--- public/myFunction.js
+++
@@ -1,8 +0,0 @@
-function updateDimensions() {
-var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-var height = window.innerHeight || document.documentElement.clientHeight ||
-document.body.clientHeight;
-document.getElementById('dimensions').innerHTML = width + 'px x ' + height + 'px';
-}
-window.onresize = updateDimensions;
-window.onload = updateDimensions;
